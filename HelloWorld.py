8f04c1c78ac030479ca97e2fbea679e842b7db01
James.Wilson <james.wilson@telent.com>
2022-07-14 15:46:18 +0100

    Removed White Space

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,4 @@
 print("Hello "+input("What is your  name? "))
 print("Wow! Only "+input("What is your age? ") + " years old!")
-<<<<<<< HEAD
 print("Hmm... Something Went Wrong...")
-=======
 print("What about this")
->>>>>>> FeatureA
